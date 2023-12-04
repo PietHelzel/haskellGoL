9f9a486b59de5da61b3f427cac3385f3c5531c76
Piet Helzel <piet.helzel@outlook.com>
2023-12-04 21:14:03 +0100

    Add function to get default ruleset

--- app/GameLogic/BoardClass.hs
+++ app/GameLogic/BoardClass.hs
@@ -3,4 +3,7 @@ module GameLogic.BoardClass where
 class BoardClass a where
     updateBoard :: a -> RuleSet -> a
 
-data RuleSet = RuleSet {death::[Integer], birth::[Integer]}
+data RuleSet = RuleSet {survive::[Integer], birth::[Integer]}
+
+getDefaultRules :: RuleSet
+getDefaultRules = RuleSet {survive = [2, 3], birth = [3]}
