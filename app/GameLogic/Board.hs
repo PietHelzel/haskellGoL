fb0b1d0a5251ae8586195f9153e68df32d8ee245
Piet Helzel <piet.helzel@outlook.com>
2023-12-04 18:08:00 +0100

    General idea of typeclass

--- app/GameLogic/Board.hs
+++ app/GameLogic/Board.hs
@@ -2,4 +2,13 @@ module GameLogic.GameLogic where
 
 import GameLogic.Cell
 
+class BoardClass a where
+    updateBoard :: a -> a
+
 newtype Board = Board [Cell]
+
+update :: Board -> Board
+update board = board
+
+instance BoardClass Board where
+    updateBoard = update
