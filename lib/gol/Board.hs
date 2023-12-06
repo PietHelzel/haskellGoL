af70c9ca0e6e534683d089003ef3249e7078e925
Piet Helzel <piet.helzel@outlook.com>
2023-12-06 20:52:08 +0100

    Deduplicate early, as indicated by faster benchmark

--- lib/gol/Board.hs
+++ lib/gol/Board.hs
@@ -13,7 +13,7 @@ data Board = Board [Cell] deriving (Show)
 
 -- | Updates the board using a ruleset.
 updateBoard :: Board -> RuleSet -> Board
-updateBoard board rules = Board $ nub [cell | cell <- cells, willExist board rules cell]
+updateBoard board rules = Board [cell | cell <- cells, willExist board rules cell]
     where cells = getAllImportantCells board
 
 -- | Checks if a cell will exist in the next generation.
@@ -28,12 +28,11 @@ willExist board@(Board cells) (RuleSet {survive=survive, birth=birth}) c
 countNeighbours :: Board -> Cell -> Integer
 countNeighbours (Board cells) c = sum [1 | n <- getNeighbours c, n `elem` cells]
 
--- TODO: Benchmark weil interessant :D (dedup vs. nicht dedup)
 -- | Gets all the cells that are currently alive as well as their neighbours.
 -- This is required because a dead cell can be born in the next generation given it has the
 -- proper number of neighbours.
 getAllImportantCells :: Board -> [Cell]
-getAllImportantCells (Board cells) = concat [cells, concat [getNeighbours c | c <- cells]]
+getAllImportantCells (Board cells) = nub $ concat [cells, concat [getNeighbours c | c <- cells]]
 
 -- | Gets all neighbouring cells of a specific cell.
 getNeighbours :: Cell -> [Cell]
