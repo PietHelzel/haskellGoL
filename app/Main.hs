8b2034712f64024029f2d1adf8cceddf45375051
flamion <contact@flamion.dev>
2023-12-11 18:28:50 +0100

    Add options parsing and help for the options

--- app/Main.hs
+++ app/Main.hs
@@ -10,8 +10,30 @@ import Data.Set (fromList)
 import Cell
 import BoardClass
 
+import Options.Applicative
+
+data Config = Config
+    { boardFilePath :: String
+    , ruleFilePath  :: String
+    } deriving Show
+
+configParser :: Parser Config
+configParser = Config
+    <$> option str 
+        (long "board"
+        <> metavar "path-to-board-file"
+        <> value ""
+        <> help "The filepath to the board file.")
+    <*> option str
+        (long "rules"
+        <> metavar "path-to-rules-file"
+        <> value ""
+        <> help "The filepath to the rules file.")
+
+
 main :: IO (AppState Board)
 main = do
+    config <- execParser opts
     runApp AppState {statePaused=False, stateBoard=
                 Board $ fromList [
                     Cell {x = 0, y = 0},
@@ -28,3 +50,8 @@ main = do
             stateTicksBetweenUpdates = 5,
             stateTicks = 0
             }
+        where 
+        opts = info (configParser <**> helper)
+            (fullDesc
+            <> progDesc "Haskell Game of Life"
+            <> header "Header here :3")
